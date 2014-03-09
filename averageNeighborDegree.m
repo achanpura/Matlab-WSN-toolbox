847b0926952346cfeccb15dd8621d8184d120c73
Ivan Brugere <ivan.brugere@gmail.com>
2014-03-09 16:21:12 -0500

    changed for function name consistency

--- averageNeighborDegree.m
+++ averageNeighborDegree.m
@@ -6,18 +6,24 @@
 % OUTPUTs: average neighbor degree vector, 1xn
 %
 % Other routines used: degrees.m, kneighbors.m
-% GB: last updated, Sep 28, 2012
+
+% Updated : Changed for function name consistency
+
+% IB: last updated, 3/9/14
 %##################################################################
 
-function ave_n_deg=aveNeighborDeg(adj)
+function av=averageNeighborDegree(adj)
 
-ave_n_deg=zeros(1,length(adj));   % initialize output vector
+av=zeros(1,length(adj));   % initialize output vector
 [deg,~,~]=degrees(adj);
 
 for i=1:length(adj)  % across all nodes
   
   neigh=kneighbors(adj,i,1);  % neighbors of i, one link away
-  if isempty(neigh); ave_n_deg(i)=0; continue; end
-  ave_n_deg(i)=sum(deg(neigh))/deg(i);
+  if isempty(neigh)
+      av(i)=0; 
+      continue; 
+  end
+  av(i)=sum(deg(neigh))/deg(i);
   
 end
